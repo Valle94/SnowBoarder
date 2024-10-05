02e560dc2e941dba0a17263e57a1f87b3d402c25
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-10-05 18:25:02 -0400

    Part 2 Creative Extension
    Added a unique sound effect and particle that only happens on a longer jump.

--- Snow Boarder/Assets/Scripts/DustTrail.cs
+++ Snow Boarder/Assets/Scripts/DustTrail.cs
@@ -1,21 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class DustTrail : MonoBehaviour
 {
     [SerializeField] ParticleSystem snowTrail;
+    [SerializeField] ParticleSystem longJump;
+    [SerializeField] AudioClip longJumpSFX;
+    [SerializeField] float airtime = 2f;
+    float hangtime = 0;
+    bool airborn = false;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
         {
+            airborn = false;
+            if (hangtime >= airtime)
+            {
+                longJump.Play();
+                GetComponent<AudioSource>().PlayOneShot(longJumpSFX);
+            }
+            hangtime = 0;
             snowTrail.Play();
         }
     }
 
     void OnCollisionExit2D(Collision2D other)
     {
+        airborn = true;
         snowTrail.Stop();
+    }
+
+    void Update()
+    {
+        if (airborn)
+        {
+            hangtime += Time.deltaTime;
+        }
     }
 }
