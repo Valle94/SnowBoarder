56376d525a1f682bdd38c8211c7d15d78449f9eb
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-10-05 17:25:55 -0400

    3.19 Prevent Double Play
    Added a bool for if we've crashed or not, and only executes crashing code (sound, respawn, control disable) if we haven't already crashed.

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,14 +9,17 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool crashed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Ground")
+        if (other.tag == "Ground" && !crashed)
         {
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
+            crashed = true;
         }
     }
 
